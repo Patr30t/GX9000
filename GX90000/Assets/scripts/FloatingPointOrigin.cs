7f04318f11ddf5923b66806dd2a98a74c74fbbe0
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-02 15:12:28 +0100

    Level Generation

---
+++ GX90000/Assets/scripts/FloatingPointOrigin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Camera))]
+public class FloatingPointOrigin : MonoBehaviour
+{
+    public float threshold = 100.0f;
+    public LevelLayoutGeneration layoutGenerator;
+
+
+    void LateUpdate()
+    {
+        Vector3 cameraPostiion = gameObject.transform.position;
+        cameraPostiion.y = 0f;
+
+        if (cameraPostiion.magnitude > threshold)
+        {
+
+            for (int z = 0; z < SceneManager.sceneCount; z++)
+            {
+                foreach (GameObject g in SceneManager.GetSceneAt(z).GetRootGameObjects())
+                {
+                    g.transform.position -= cameraPostiion;
+                }
+            }
+            Vector3 originDelta  = Vector3.zero - cameraPostiion;
+            layoutGenerator.UpdateSpawnOrigin(originDelta);
+            Debug.Log("recentering, origin delta = " + originDelta);
+
+        }
+    }
+}
