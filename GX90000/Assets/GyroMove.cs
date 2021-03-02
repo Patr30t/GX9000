2b358ac03b5b10925b215b913dd80df188674f74
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-02 09:36:25 +0100

    adding the Gyro movement and removing the accelerometer

---
+++ GX90000/Assets/GyroMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroMove : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+
+    [SerializeField]
+    private float upSpeed = 2f;
+
+    [SerializeField]
+    private float maxSpeed = 20f;
+    private  Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Input.gyro.enabled = true;
+    }
+
+    private void Update() {
+        {
+            Quaternion deviceRotation = DeviceRotation.Get();
+
+            transform.rotation = deviceRotation;
+            
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+    }
+}
