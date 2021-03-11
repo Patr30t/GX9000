a3751ccf082bf544e5814fbd277903925e065a8c
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-11 11:14:23 +0100

    adding scriptable object for ground generatoion

---
+++ GX90000/Assets/inter/LootCrate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootCrate : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        Debug.Log("interacting with crate");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
