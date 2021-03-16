1f2fc8e6a4598145b3acbff9f52da6d5448ae2ec
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-16 23:14:01 +0100

    more scripts added

---
+++ GX90000/Assets/scripts/GyroMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroMovement : MonoBehaviour
+{
+
+    public UnityEngine.Gyroscope gyro;
+   
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            Input.gyro.enabled = true;
+            
+        }
+    }
+
+    void Update() {
+        
+    
+        if(SystemInfo.supportsGyroscope)
+        {
+            if (SystemInfo.supportsGyroscope)
+            transform.rotation = GyroToUnity(Input.gyro.attitude);
+        }
+
+         Quaternion GyroToUnity (Quaternion q) {
+            {
+                return new Quaternion(q.x, q.y, -q.z, -q.w);
+            }
+        }
+
+      
+}
+}
